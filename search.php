335e63cf2be9df26ce3e68e5271d5a8ca96be646
somsaksamart <50091048+somsaksamart@users.noreply.github.com>
2019-04-29 16:31:14 +0700

    Add files via upload

--- search.php
+++ search.php
@@ -2,7 +2,8 @@
   require('connect_db.php');
   $sql_text = "SELECT * FROM catbot WHERE keyword LIKE '%b%'";
   $query = mysqli_query($conn,$sql_text);
-  $numrow = mysqli_numrows($query);
+  $obj = mysqli_fetch_array($query);
+  $numrow = mysqli_numrows($obj);
   while($objresult = mysqli_fetch_assoc($query))
   {
       echo $objresult['answer']."<br>";
