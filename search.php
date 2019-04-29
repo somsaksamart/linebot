91973b1d8aaad6c14a14b70734daed9d61591004
somsaksamart <50091048+somsaksamart@users.noreply.github.com>
2019-04-29 15:51:01 +0700

    Add files via upload

---
+++ search.php
@@ -0,0 +1,12 @@
+<?php
+  require('connet_db.php');
+  $sql_text ="SELECT * FROM catbot WHERE keyword LIKE 'a'"
+  $query = mysqli_query($conn,$sql_text);
+  while($objresult = mysqli_fetch_assoc($query))
+  {
+      echo $objresult['answer']."<br>";
+  }
+
+
+
+?>
